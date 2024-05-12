7d23773f0530db80cae2dd41727fdd63d1f99340
Kabir4874 <kabir.cse.bd@gmail.com>
2024-05-12 19:30:32 +0600

    add

--- src/components/home/HighlightText.jsx
+++ src/components/home/HighlightText.jsx
@@ -1,10 +1,8 @@
-
-
 const HighlightText = () => {
   return (
-    <div className="w-full flex flex-col items-center justify-center gap-[2.05rem] my-[7.75rem]">
+    <div className="w-full flex flex-col items-center justify-center gap-[2.05rem] my-[7.75rem] ">
       <div className="w-[75rem] h-[0.0625rem] hr-bg" />
-      <h2 className="text-center font-syne text-[7.3125rem] font-bold highlight-text text-nowrap">
+      <h2 className="text-center font-syne text-[7.3125rem] font-bold text-nowrap words highlight-text">
         Build Your website with softlight Template Today.
       </h2>
       <div className="w-[75rem] h-[0.0625rem] hr-bg" />
